7ea0284c7e6ca59e516eb671b82dc23629539aa2
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2024-02-24 23:00:30 -0600

    add example

---
+++ app/src/main/java/com/example/linearlayout08012024/Example2.kt
@@ -0,0 +1,11 @@
+package com.example.linearlayout08012024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Example2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example2)
+    }
+}
